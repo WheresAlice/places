214259d490b82f49b4d95c6e646369f0f874471b
wheresalice <wheresalice@users.noreply.github.com>
2019-05-20 12:18:08 +0100

    color coding

--- js/site.js
+++ js/site.js
@@ -5,9 +5,26 @@ var osmAttrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</
 var osm = new L.TileLayer(osmUrl, {minZoom: 2, maxZoom: 12, attribution: osmAttrib});
 mymap.addLayer(osm);
 
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML += '<i style="background: #00008B"></i> Visited<br>';
+    div.innerHTML += '<i style="background: DeepSkyBlue"></i> To Visit<br>';
+    div.innerHTML += '<i style="background: PaleGreen"></i> Your Suggestions<br>';
+    return div;
+};
+
+legend.addTo(mymap);
+
 function onEachFeature(feature, layer) {
     if (feature.properties && feature.properties.name) {
         layer.bindPopup(feature.properties.name);
+    }
+    if (feature.properties && feature.properties._umap_options && feature.properties._umap_options.color) {
+        layer.setIcon(new L.DivIcon.SVGIcon({fillColor: feature.properties._umap_options.color, fillOpacity: 1, iconSize:  	[24,32]}));
+    } else {
+        layer.setIcon(new L.DivIcon.SVGIcon({fillColor: '#00008B', fillOpacity: 1, iconSize:  	[24,32]}))
     }
 }
 
