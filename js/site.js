5afff271baea22341518a6123d91f1ca9102845d
wheresalice <wheresalice@users.noreply.github.com>
2019-05-20 12:47:44 +0100

    enable submissions

--- js/site.js
+++ js/site.js
@@ -1,7 +1,7 @@
 var mymap = L.map('mapid').fitWorld();
 
 var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+var osmAttrib = 'Map data ï¿½ <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
 var osm = new L.TileLayer(osmUrl, {minZoom: 2, maxZoom: 12, attribution: osmAttrib});
 mymap.addLayer(osm);
 
@@ -11,7 +11,7 @@ legend.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'info legend');
     div.innerHTML += '<i style="background: #00008B"></i> Visited<br>';
     div.innerHTML += '<i style="background: DeepSkyBlue"></i> Not Visited<br>';
-    div.innerHTML += '<i style="background: PaleGreen"></i> Your Suggestions<br>';
+    div.innerHTML += '<i style="background: PaleGreen"></i> <a href="/submit.html">Your Suggestions</a><br>';
     return div;
 };
 
