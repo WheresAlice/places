08669515baef9d7949b1060ed3aa0a84f7528f60
wheresalice <wheresalice@users.noreply.github.com>
2019-05-21 10:37:11 +0100

    add ability to replace markers

--- js/submit.js
+++ js/submit.js
@@ -1,26 +1,31 @@
-var mymap = L.map('submitmap').fitWorld();
+const myMap = L.map('submitmap').fitWorld();
 
-var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib = 'Map data � <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-var osm = new L.TileLayer(osmUrl, {minZoom: 2, maxZoom: 18, attribution: osmAttrib});
-mymap.addLayer(osm);
+const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const osmAttrib = 'Map data � <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+const osm = new L.TileLayer(osmUrl, {minZoom: 2, maxZoom: 18, attribution: osmAttrib});
+myMap.addLayer(osm);
 
-var marker;
-mymap.on('click', onMapClick);
+let marker;
+myMap.on('click', onMapClick);
 function onMapClick(e) {
-    if (Object.keys(mymap._layers).length === 1) {
+    if (Object.keys(myMap._layers).length > 1) {
+        deleteMarker()
+    }
+    if (Object.keys(myMap._layers).length === 1) {
         marker = new L.marker(e.latlng, {draggable: false});
-        mymap.addLayer(marker);
+        myMap.addLayer(marker);
         marker.on("click", onMarkerClick);
         document.getElementById('pointlocation').value = JSON.stringify(marker.toGeoJSON());
     }
 }
 
-function onMarkerClick() {
-    mymap.removeLayer(marker);
+function deleteMarker() {
+    myMap.removeLayer(marker);
     document.getElementById('pointlocation').value = '';
 }
 
+function onMarkerClick() {
+    deleteMarker();
+}
 
-
-mymap.setView([35.5754390657896, 45.388114638626575], 2);
+myMap.setView([35.5754390657896, 45.388114638626575], 2);
